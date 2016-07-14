f8dc24ea8807a5f079a6c8234e5e6e8b36432bb9
ihugojimenez <hugofernando809@hotmail.com>
2016-07-14 15:53:04 -0400

    DALL

---
+++ AlmacenTech/DALL/AlmacenBancasDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL
+{
+    public class AlmacenBancasDB : DbContext
+    {
+        public AlmacenBancasDB() : base("name = AlmacenDb") 
+        {
+            
+        }
+    }
+}
